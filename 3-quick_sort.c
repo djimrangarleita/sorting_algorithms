1b019eec4fe12542b953a7c56d9c93a0d774d7cc
Djimra NGARLEITA <dngarleita@gmail.com>
2024-02-20 23:52:25 +0300

    fix: betty and format big O

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,48 +11,63 @@ void quick_sort(int *array, size_t size)
 	if (size <= 0 || array == NULL)
 		return;
 
-	lomuto_partition(array, 0, size - 1, size);
+	quicksort(array, 0, size - 1, size);
 }
 
 /**
- * partition - implementation of lomuto partitioning to sort elts
+ * lomuto_partition - implementation of lomuto partitioning to sort elts
  * @array: subarray to be sorted
  * @low: lowest index of the subarray
  * @high: highest index of the subarray
  * @size: size of the array for printing
  * Return: index of the last pivot
  */
-int *lomuto_partition(int *array, int low, int high, size_t size)
+int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int i, j;
 	int pivot, tmp;
 
-	if (high <= low)
-	{
-		return (array);
-	}
-
 	pivot = array[high];
-	j = low;
+	j = low - 1;
 
 	for (i = low; i < high; i++)
 	{
 		if (array[i] <= pivot)
 		{
+			j++;
 			tmp = array[i];
 			array[i] = array[j];
 			array[j] = tmp;
-			j++;
 		}
 	}
 
+	j++;
 	tmp = array[high];
 	array[high] = array[j];
 	array[j] = tmp;
 	print_array(array, size);
 
-	lomuto_partition(array, low, j - 1, size);
-	lomuto_partition(array, j + 1, high, size);
+	return (j);
+}
 
-	return (array);
+/**
+ * quicksort - recursive part of the algo
+ * @array: the array to be sorted
+ * @low: lowest idx of subarray
+ * @high: highest index of subarray
+ * @size: total size of the array
+ * Return: nothing
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int p;
+
+	if (low >= high || low < 0)
+	{
+		return;
+	}
+
+	p = lomuto_partition(array, low, high, size);
+	quicksort(array, low, p - 1, size);
+	quicksort(array, p + 1, high, size);
 }
--- sort.h
+++ sort.h
@@ -25,6 +25,7 @@ listint_t *create_listint(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap_nodes(listint_t *n1, listint_t *n2);
-int *lomuto_partition(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
 
 #endif
